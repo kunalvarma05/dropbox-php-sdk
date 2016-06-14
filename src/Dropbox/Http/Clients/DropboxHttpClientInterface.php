c4e374e3e8dd2dfe919835aefb2a4e3fc9ef89f9
Kunal Varma <kunalvarma05@gmail.com>
2016-06-15 04:19:38 +0530

    DropboxHttpClientInterface

---
+++ src/Dropbox/Http/Clients/DropboxHttpClientInterface.php
@@ -0,0 +1,19 @@
+<?php
+namespace Kunnu\Dropbox\Http\Clients;
+
+interface DropboxHttpClientInterface
+{
+    /**
+     * Send request to the server and fetch the raw response
+     * @param  string $url     URL/Endpoint to send the request to
+     * @param  string $method  Request Method
+     * @param  string|resource|StreamInterface $body Request Body
+     * @param  array  $headers Request Headers
+     * @param  array  $options Additional Options
+     *
+     * @return \Kunnu\Dropbox\Http\DropboxRawResponse Raw response from the server
+     *
+     * @throws \Kunnu\Dropbox\Exceptions\DropboxClientException
+     */
+    public function send($url, $method, $body, $headers = [], $options = []);
+}
