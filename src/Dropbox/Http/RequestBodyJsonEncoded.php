19d6742254c4476b48aae1b16398acfcb2edca03
Kunal Varma <kunalvarma05@gmail.com>
2016-06-15 04:23:12 +0530

    RequestBodyJsonEncoded

---
+++ src/Dropbox/Http/RequestBodyJsonEncoded.php
@@ -0,0 +1,34 @@
+<?php
+namespace Kunnu\Dropbox\Http;
+
+class RequestBodyJsonEncoded implements RequestBodyInterface
+{
+
+    /**
+     * Request Params
+     *
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * Create a new RequestBodyJsonEncoded instance
+     *
+     * @param array $params Request Params
+     */
+    public function __construct(array $params = [])
+    {
+        $this->params = $params;
+    }
+
+    /**
+     * Get the Body of the Request
+     *
+     * @return string
+     */
+    public function getBody()
+    {
+        return json_encode($this->params);
+    }
+
+}
