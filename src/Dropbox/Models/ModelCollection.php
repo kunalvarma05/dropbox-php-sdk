f45c7c2e32119fd192cb80550f51add14451360b
Kunal Varma <kunalvarma05@gmail.com>
2016-06-16 12:21:37 +0530

    Collection Docblock Link

--- src/Dropbox/Models/ModelCollection.php
+++ src/Dropbox/Models/ModelCollection.php
@@ -3,6 +3,9 @@ namespace Kunnu\Dropbox\Models;
 
 use Illuminate\Support\Collection;
 
+/**
+ * @link https://laravel.com/docs/5.2/collections
+ */
 class ModelCollection extends Collection
 {
     //
