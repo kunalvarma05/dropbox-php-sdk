4c3a86e793fa63bda30d31c725b0c11e16578b29
Kunal Varma <kunalvarma05@gmail.com>
2016-06-24 21:49:07 +0530

    Thumbnail model extends File model

--- src/Dropbox/Models/Thumbnail.php
+++ src/Dropbox/Models/Thumbnail.php
@@ -1,56 +1,7 @@
 <?php
 namespace Kunnu\Dropbox\Models;
 
-class Thumbnail extends BaseModel
+class Thumbnail extends File
 {
-
-    /**
-     * The file contents
-     *
-     * @var string
-     */
-    protected $contents;
-
-    /**
-     * File Metadata
-     *
-     * @var \Kunnu\Dropbox\Models\FileMetadata
-     */
-    protected $metadata;
-
-
-    /**
-     * Create a new Thumbnail instance
-     *
-     * @param array  $data
-     * @param string $contents
-     */
-    public function __construct(array $data, $contents)
-    {
-        parent::__construct($data);
-        $this->contents = $contents;
-        $this->metadata = new FileMetadata($data);
-    }
-
-    /**
-     * The metadata for the file
-     *
-     * @return \Kunnu\Dropbox\Models\FileMetadata
-     */
-    public function getMetadata()
-    {
-        return $this->metadata;
-    }
-
-    /**
-     * Get the file contents
-     *
-     * @return string
-     */
-    public function getContents()
-    {
-        return $this->contents;
-    }
-
 
 }
